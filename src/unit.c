9ab87804eba598b4b0c3de9150fc968c1a4f9461
Steven Arnow <s@rdw.se>
2019-05-31 00:25:53 +0200

    somehow this will become a unit manager

--- src/unit.c
+++ src/unit.c
@@ -1,7 +1,79 @@
+#include "main.h"
 #include "unit.h"
 
 
 
-int unit_add(int team, enum UnitType type, int x, int y) {
+void unit_housekeeping() {
+	int i;
+	struct UnitEntry **e, *tmp;
+
+	for (i = 0; i < MAX_TEAM; i++) {
+		d_util_mutex_lock(s->team[i].unit.lock);
+		
+		for (e = &s->team[i].unit.unit; *e; e = &(*e)->next) {
+			if ((*e)->delete_flag) {
+				tmp = *e;
+				*e = (*e)->next;
+				free(tmp);
+			}
+		}
+		
+		d_util_mutex_unlock(s->team[i].unit.lock);
+	}
+}
+
+
+void unit_delete(int team, int index) {
+	struct UnitEntry **e;
+
+	d_util_mutex_lock(s->team[team].unit.lock);
+	for (e = &s->team[team].unit.unit; *e; e = &(*e)->next) {
+		if ((*e)->map_index == index)
+			continue;
+		(*e)->delete_flag = 1;
+		break;
+	}
 	
+	d_util_mutex_unlock(s->team[team].unit.lock);
+}
+
+
+void unit_add(int team, enum UnitType type, int x, int y) {
+	int index, id;
+	struct UnitEntry *e;
+
+	d_util_mutex_lock(s->team[team].unit.lock);
+
+	if (x < 0 || y < 0)
+		goto fail;
+	if (x >= s->active_level->layer->tilemap->w || y >= s->active_level->layer->tilemap->h)
+		goto fail;
+	
+	index = x + y * s->active_level->layer->tilemap->w;
+
+	if ((s->active_level->layer->tilemap->data[index] & TILESET_MASK) >= TILESET_UNIT_BASE)
+		goto fail; // Unit already there
+	e = malloc(sizeof(*e));
+	e->create_flag = 1;
+	e->modify_flag = 0;
+	e->delete_flag = 0;
+	e->previous_tile = s->active_level->layer->tilemap->data[index];
+	s->active_level->layer->tilemap->data[index] = TILESET_UNIT_BASE + TILESET_TEAM_STEP * team;
+	e->map_index = index;
+	e->type = type;
+	e->next = s->team[team].unit.unit;
+	s->team[team].unit.unit = e;
+	
+fail:
+	d_util_mutex_unlock(s->team[team].unit.lock);
+	return;	
+}
+
+
+void unit_init() {
+	int i;
+
+	for (i = 0; i < MAX_TEAM; i++)
+		s->team[i].unit.lock = d_util_mutex_create();
+	return;
 }
--- src/unit.h
+++ src/unit.h
@@ -1,6 +1,11 @@
 #ifndef UNIT_H_
 #define	UNIT_H_
 
+#define	TILESET_UNIT_BASE		64
+#define	TILESET_TEAM_STEP		16
+#define	TILESET_MASK			0xFFF
+
+#include <darnit/darnit.h>
 
 enum UnitType {
 	UNIT_TYPE_GENERATOR,
@@ -12,14 +17,26 @@ enum UnitType {
 
 struct UnitEntry {
 	int				map_index;
+	unsigned int			previous_tile;
 	enum UnitType			type;
+	struct UnitEntry		*next;
+
+	int				create_flag;
+	int				modify_flag;
+	int				delete_flag;
 };
 
 
 struct Unit {
+	DARNIT_MUTEX			*lock;
+
 	struct UnitEntry		*unit;
-	int				units;
 };
+
+
+void unit_init();
+void unit_add(int team, enum UnitType type, int x, int y);
+void unit_delete(int team, int index);
 
 
 #endif
