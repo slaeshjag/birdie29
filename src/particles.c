59b9833a62e7ac07d1de363913897b8dd9742f29
Steven Arnow <s@rdw.se>
2019-06-01 10:58:56 +0200

    Explosion particles

--- src/particles.c
+++ src/particles.c
@@ -54,6 +54,17 @@ void particle_add(enum ParticleType type, int x, int y, int angle) {
 		d_particle_emitter_angle(pe->particle, 0, 3600);
 		d_particle_point_size(pe->particle, 10);
 		d_particle_pulse(pe->particle);
+	} else if (type == PARTICLE_TYPE_EXPLOSION) {
+		pe->particle = d_particle_new(1000, DARNIT_PARTICLE_TYPE_POINT);
+		d_particle_color_start(pe->particle, 255, 255, 0, 255);
+		d_particle_color_target(pe->particle, 128, 32, 32, 0);
+		d_particle_life(pe->particle, 1000);
+		d_particle_mode(pe->particle, DARNIT_PARTICLE_MODE_PULSAR);
+		d_particle_emitter_velocity(pe->particle, 100, 1500);
+		d_particle_emitter_move(pe->particle, x, y);
+		d_particle_emitter_angle(pe->particle, 0, 3600);
+		d_particle_point_size(pe->particle, 10);
+		d_particle_pulse(pe->particle);
 	} else {
 		free(pe);
 		return;
